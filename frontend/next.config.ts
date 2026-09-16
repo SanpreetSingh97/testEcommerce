31fef68034542998814af20d125ddf667216acd1
Sanpreet97 <singhsanpreet017@gmail.com>
2026-09-16 22:04:52 +0530

    fourth commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
+import os from "os";
+
+function getLocalNetworkIPs(): string[] {
+  const ips: string[] = [];
+  for (const addrs of Object.values(os.networkInterfaces())) {
+    for (const addr of addrs ?? []) {
+      if (addr.family === "IPv4" && !addr.internal) {
+        ips.push(addr.address);
+      }
+    }
+  }
+  return ips;
+}
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: getLocalNetworkIPs(),
   images: {
     remotePatterns: [
       {
